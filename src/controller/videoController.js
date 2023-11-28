afb662bb8c908436d451975d476f46c2587230cb
Juhye O <artigen3@gmail.com>
2023-11-28 14:33:04 +0900

    CSS basic added

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -43,7 +43,7 @@ export const postEdit = async (req, res)=>{
 };
 
 export const home = async (req, res) => { 
-    const videos = await Video.find({});
+    const videos = await Video.find({}).populate("owner");
     return res.render("home", {pageTitle: "HOME", videos});
 };
 
@@ -55,7 +55,7 @@ export const search = async (req, res) => {
             title: {
                 $regex: new RegExp(keyword, "i")
             },
-        });
+        }).populate("owner");
     }
     return res.render("search", {pageTitle: "Search", videos});
 };

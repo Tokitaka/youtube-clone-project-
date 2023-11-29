3fee45c834f60f3fc063771e74120343a9a27ecd
Juhye O <artigen3@gmail.com>
2023-11-29 18:12:00 +0900

    Video Recorder record action added

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,6 +1,9 @@
 const startBtn = document.getElementById("startBtn");
 const videoPreview= document.getElementById("preview");
 
+let stream;
+let recorder;
+
 //preview 
 const init= async ()=>{
 	const stream = await navigator.mediaDevices.getUserMedia(
@@ -12,5 +15,35 @@ const init= async ()=>{
 	videoPreview.play(); 	
 };
 init();
+
+const handleStart = ()=>{
+	startBtn.innerText = "Stop Recording";
+	startBtn.removeEventListener("click", handleStart);
+	startBtn.addEventListener("click", handleStop);
+	//start recording
+	recorder = new MediaRecorder(stream);
+	//dataavailable event handler 등록 
+	recorder.ondataavailable = (event) => {
+		event.data; // -> file recorded
+		const videoFile = URL.createObjectURL(event.data);
+		// preview 제거 
+		videoPreview.srcObject = null;
+		// 녹화된 video 재생 
+		videoPreview.src = videoFile;
+		videoPreview.loop = true;
+		videoPreview.play();
+};
+	recorder.start();
+};
+const handleDownload = ()=>{
+};
+const handleStop = ()=>{
+	startBtn.innerText = "Download Recording";
+	startBtn.removeEventListener("click", handleStop);
+	startBtn.addEventListener("click", handleDownload);
+	//stop recording
+	recorder.stop();
+	
+};
 
 startBtn.addEventListener("click", handleStart);

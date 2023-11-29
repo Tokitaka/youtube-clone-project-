ad287ec6e1ecd35c87285406db100e7f9ae64fad
Juhye O <artigen3@gmail.com>
2023-11-29 13:31:20 +0900

    video player css styles & keydown, video click play function added

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,14 +1,20 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
+const playBtnIcon = playBtn.querySelector("i");
 const muteBtn = document.getElementById("mute");
+const muteBtnIcon = muteBtn.querySelector("i");
 const volumeRange = document.getElementById("volume");
 const currentTIme = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
 const timeline = document.getElementById("timeline");
 const fullScreenBtn = document.getElementById("fullScreen");
+const fullScreenIcon = fullScreenBtn.querySelector("i");
 const videoContainer = document.getElementById("videoContainer");
+const videoControls = document.getElementById("videoControls");
 
 //global variable 
+let ctrStopTimeout = null;
+let ctrLeaveTimeout = null;
 let volumeValue  = 0.5;
 video.volume = volumeValue; // 실제 볼륨 , range - value 와 맞춰주기 
 
@@ -18,7 +24,7 @@ const handlePlayClick = () => {
     } else {
         video.pause();
     }
-    playBtn.innerText = video.paused ? "Play" : "Pause";
+    playBtnIcon.classList = video.paused ? "fas fa-play" : "fas fa-pause";
 };
 
 const handleMuteClick = () => {
@@ -27,7 +33,7 @@ const handleMuteClick = () => {
     } else {
         video.muted = true;
     }
-    muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+    muteBtnIcon.classList = video.muted ? "fas fa-volume-mute" : "fas fa-volume-up";
     volumeRange.value = video.muted ? 0 : volumeValue;
 };
 const handleVolumeChange =  (event) => {
@@ -65,14 +71,36 @@ const handleFullScreen = () => {
     } else {
         videoContainer.requestFullscreen();
     }
-    fullScreenBtn.innerText = document.fullscreenElement ? "Enter FullScreen" : "Exit FullScreen";
+    fullScreenIcon.classList = fullscreen ? "fas fa-expand" : "fas fa-compress";
 
 };
-//비디오 시간이 변결될 떄 
+const hideControls = () => videoControls.classList.remove("showing");
+// mouse 움직일 때 control 보여주기 
+const handleMouseMove = () => { 
+    if(ctrLeaveTimeout){
+    clearTimeout(ctrLeaveTimeout); 
+    controlsTimeout = null;
+    }
+    if(ctrStopTimeout){
+        clearTimeout(ctrStopTimeout); 
+        controlsTimeout = null;
+        }
+    videoControls.classList.add("showing");
+    ctrStopTimeout = setTimeout(hideControls,2000);
+};
+const handleMouseLeave = () => {
+    ctrLeaveTimeout = setTimeout(hideControls,2000);
+};
+
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMuteClick);
 volumeRange.addEventListener("input", handleVolumeChange); // value 변화 감지 
 video.addEventListener("loadedmetadata", handleLoadedMetadata);
 video.addEventListener("timeupdate", handleTimeUpdate);
+video.addEventListener("click", handlePlayClick);
 timeline.addEventListener("input", handleTimelineChange);
 fullScreenBtn.addEventListener("click", handleFullScreen);
+videoContainer.addEventListener("mousemove", handleMouseMove);
+videoContainer.addEventListener("mouseleave", handleMouseLeave);
+document.addEventListener("keydown",handlePlayClick);
+
